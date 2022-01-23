9d886674a21686c7b82b0fb0768b8cd32a83b02b
yoeik <joaquingon97@gmail.com>
2022-01-23 13:01:28 -0300

    Cambios deploy 3

---
+++ server.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files
+app.use(express.static(__dirname + '/portfolio'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/outputPath/index.html'));
+});
+
+// default Heroku port
+app.listen(process.env.PORT || 5000)
